c7dbc56434c38b81cb8e86035607d2cf5b869cdd
amitbidlan <asianbarrow@gmail.com>
2026-01-18 22:48:30 +0900

    Initialized again

---
+++ dsa/9_Palindrome_Number.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func isPalindrome(x int) bool {
+	s := strconv.Itoa(x);
+	l,r := 0 , len(s)-1;
+	for l < r {
+		if s[l] != s[r] {
+			return false;
+		}
+		l++;
+		r--;
+	}
+	return true;
+}
+
+func main() {
+	fmt.Println(isPalindrome(121)) 
+	fmt.Println(isPalindrome(123)) 
+}
